19fa25d6b1b9dc8e2509cfae373cea7e45774199
Syanmil <syanmil@yahoo.com>
2017-02-12 11:16:24 +0700

    creating controller

---
+++ controller/memoController.js
@@ -0,0 +1,18 @@
+const memos = require('../models/memo')
+
+let memoController = {
+  create: function () {
+
+  },
+  read: function(){
+
+  },
+  update: function(){
+
+  },
+  delete: function(){
+
+  }
+}
+
+module.exports = memoController
