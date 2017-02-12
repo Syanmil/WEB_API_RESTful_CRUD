07dc757cde0b01e4fbbec3df91db2144ac3374ec
Syanmil <syanmil@yahoo.com>
2017-02-12 11:29:00 +0700

    create function done

--- controllers/memoController.js
+++ controllers/memoController.js
@@ -2,11 +2,16 @@ const memos = require('../models').memo
 
 let memoController = {
   create: function (req, res) {
-
+    let content =  req.body.content
+    memos.create({
+      content: content
+    }).then(function(memo){
+      res.send(memo)
+    })
   },
   read: function(req, res){
-    memos.findAll().then(function(data) {
-      res.send(data)
+    memos.findAll().then(function(memos) {
+      res.send(memos)
     })
   },
   update: function(req, res){
--- routes/memos.js
+++ routes/memos.js
@@ -3,5 +3,9 @@ var router = express.Router();
 var memoController = require('../controllers/memoController')
 
 router.get('/', memoController.read);
+router.post('/', memoController.create);
+router.put('/:id', memoController.update);
+router.delete('/:id', memoController.delete);
+
 
 module.exports = router;
