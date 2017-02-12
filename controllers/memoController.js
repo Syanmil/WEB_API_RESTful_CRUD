78357fa5d62a860f2f8ecb51e0913f6f1bdb7475
Syanmil <syanmil@yahoo.com>
2017-02-12 11:53:01 +0700

    update and delete function done

--- controllers/memoController.js
+++ controllers/memoController.js
@@ -15,10 +15,35 @@ let memoController = {
     })
   },
   update: function(req, res){
-
+    let id = req.params.id
+    let content = req.body.content
+    memos.update({
+      content : content
+    }, {
+      where: {
+      id: id
+      }
+    }).then(function(data){
+      if(data[0] == 0){
+        res.send('fail')
+      } else {
+        res.send('success')
+      }
+    })
   },
   delete: function(req, res){
-
+    let id = req.params.id
+    memos.findOne(
+      {where: {
+        id: id
+      }}
+    ).then(function(memo){
+      memo.destroy()
+    }).then(function(){
+      res.send('success')
+    }).catch(function(err){
+      res.send('fail')
+    })
   }
 }
 
